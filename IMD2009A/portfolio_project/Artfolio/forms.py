549978b744d52a94ef56da1bd7062c1bd024127a
Rachel <racheldewolfe@cmail.carleton.ca>
2024-02-26 20:44:50 -0500

    added all fields for template into model and text fields to form

--- IMD2009A/portfolio_project/Artfolio/forms.py
+++ IMD2009A/portfolio_project/Artfolio/forms.py
@@ -5,5 +5,8 @@ from .models import template
 class templateForm(forms.ModelForm):
     class Meta:
         model = template
-        fields = ['project1Title', 'project2Title',
+        fields = ['project1Title', 'p1FinalCap', 'p1ProgressCap1', 'p1ProgressCap2', 'p1ProgressCap3',
+                  'project2Title', 'p2FinalCap', 'p2ProgressCap1', 'p2ProgressCap2', 'p2ProgressCap3',
+                  'project3Title', 'p3FinalCap', 'p3ProgressCap1', 'p3ProgressCap2', 'p3ProgressCap3',
+                  'project4Title', 'p4FinalCap', 'p4ProgressCap1', 'p4ProgressCap2', 'p4ProgressCap3',
                   ]
--- IMD2009A/portfolio_project/Artfolio/models.py
+++ IMD2009A/portfolio_project/Artfolio/models.py
@@ -11,7 +11,42 @@ from django.db import models
 
 
 class template(models.Model):
-    project1Title = models.CharField(max_length=100, default="Title1")
-    project2Title = models.CharField(max_length=100, default="Title2")
+    project1Title = models.CharField(max_length=100, default="Project 1 Title")
+    p1FinalImg = models.ImageField
+    p1FinalCap = models.CharField(max_length=50, default="final product caption")
+    p1ProgressImg1 = models.ImageField
+    p1ProgressImg2 = models.ImageField
+    p1ProgressImg3 = models.ImageField
+    p1ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
+    p1ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
+    p1ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
 
+    project2Title = models.CharField(max_length=100, default="Project 2 Title")
+    p2FinalImg = models.ImageField
+    p2FinalCap = models.CharField(max_length=50, default="final product caption")
+    p2ProgressImg1 = models.ImageField
+    p2ProgressImg2 = models.ImageField
+    p2ProgressImg3 = models.ImageField
+    p2ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
+    p2ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
+    p2ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
 
+    project3Title = models.CharField(max_length=100, default="Project 3 Title")
+    p3FinalImg = models.ImageField
+    p3FinalCap = models.CharField(max_length=50, default="final product caption")
+    p3ProgressImg1 = models.ImageField
+    p3ProgressImg2 = models.ImageField
+    p3ProgressImg3 = models.ImageField
+    p3ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
+    p3ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
+    p3ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
+
+    project4Title = models.CharField(max_length=100, default="Project 4 Title")
+    p4FinalImg = models.ImageField
+    p4FinalCap = models.CharField(max_length=50, default="final product caption")
+    p4ProgressImg1 = models.ImageField
+    p4ProgressImg2 = models.ImageField
+    p4ProgressImg3 = models.ImageField
+    p4ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
+    p4ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
+    p4ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
