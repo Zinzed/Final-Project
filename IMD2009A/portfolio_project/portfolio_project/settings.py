3cb4a126423b73450a98aba2e1b600dde595e6dd
naomileron <leronnaomi@gmail.com>
2024-02-28 16:37:08 -0500

    added html for editTemplate and newTemplate pages

--- IMD2009A/portfolio_project/portfolio_project/settings.py
+++ IMD2009A/portfolio_project/portfolio_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'Artfolio/static'
+    BASE_DIR /'Artfolio/static'
 ]
+STATIC_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
