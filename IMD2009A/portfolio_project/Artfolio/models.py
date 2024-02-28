5425a9dd90b39f3aa0064dd9d71b770d26756a94
Zineb Amezyane <zineb.amezyane0@gmail.com>
2024-02-28 11:55:26 -0500

    Edited models.py and forms.py

--- IMD2009A/portfolio_project/Artfolio/models.py
+++ IMD2009A/portfolio_project/Artfolio/models.py
@@ -12,44 +12,44 @@ from django.db import models
 
 class template(models.Model):
     project1Title = models.CharField(max_length=100, default="Project 1 Title")
-    p1FinalImg = models.ImageField
+    p1FinalImg = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p1FinalCap = models.CharField(max_length=50, default="final product caption")
-    p1ProgressImg1 = models.ImageField
-    p1ProgressImg2 = models.ImageField
-    p1ProgressImg3 = models.ImageField
+    p1ProgressImg1 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p1ProgressImg2 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p1ProgressImg3 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p1ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
     p1ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
     p1ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
     p1Explanation = models.CharField(max_length=300, default="short explanation of your design process")
 
     project2Title = models.CharField(max_length=100, default="Project 2 Title")
-    p2FinalImg = models.ImageField
+    p2FinalImg = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p2FinalCap = models.CharField(max_length=50, default="final product caption")
-    p2ProgressImg1 = models.ImageField
-    p2ProgressImg2 = models.ImageField
-    p2ProgressImg3 = models.ImageField
+    p2ProgressImg1 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p2ProgressImg2 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p2ProgressImg3 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p2ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
     p2ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
     p2ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
     p2Explanation = models.CharField(max_length=300, default="short explanation of your design process")
 
     project3Title = models.CharField(max_length=100, default="Project 3 Title")
-    p3FinalImg = models.ImageField
+    p3FinalImg = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p3FinalCap = models.CharField(max_length=50, default="final product caption")
-    p3ProgressImg1 = models.ImageField
-    p3ProgressImg2 = models.ImageField
-    p3ProgressImg3 = models.ImageField
+    p3ProgressImg1 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p3ProgressImg2 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p3ProgressImg3 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p3ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
     p3ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
     p3ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
     p3Explanation = models.CharField(max_length=300, default="short explanation of your design process")
 
     project4Title = models.CharField(max_length=100, default="Project 4 Title")
-    p4FinalImg = models.ImageField
+    p4FinalImg = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p4FinalCap = models.CharField(max_length=50, default="final product caption")
-    p4ProgressImg1 = models.ImageField
-    p4ProgressImg2 = models.ImageField
-    p4ProgressImg3 = models.ImageField
+    p4ProgressImg1 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p4ProgressImg2 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
+    p4ProgressImg3 = models.ImageField(upload_to='projectImages/', null=True, blank=True)
     p4ProgressCap1 = models.CharField(max_length=50, default="progress image 1 caption")
     p4ProgressCap2 = models.CharField(max_length=50, default="progress image 2 caption")
     p4ProgressCap3 = models.CharField(max_length=50, default="progress image 3 caption")
