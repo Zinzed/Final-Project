06506c9094fbf0a4851160be83efcc0bd805140f
Zinzed <zineb.amezyane0@gmail.com>
2024-03-18 13:57:00 -0400

    Created user registration and login page.

--- IMD2009A/portfolio_project/Artfolio/forms.py
+++ IMD2009A/portfolio_project/Artfolio/forms.py
@@ -1,5 +1,22 @@
 from django import forms
 from .models import template
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput, TextInput
+
+#Registration
+class userForm(UserCreationForm):
+    class Meta:
+
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+#Authentication
+
+class loginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
 
 
 class templateForm(forms.ModelForm):
@@ -11,4 +28,5 @@ class templateForm(forms.ModelForm):
             'project3Title', 'p3FinalImg', 'p3FinalCap', 'p3ProgressImg1', 'p3ProgressCap1', 'p3ProgressImg2', 'p3ProgressCap2', 'p3ProgressImg3', 'p3ProgressCap3', 'p3Explanation',
             'project4Title', 'p4FinalImg', 'p4FinalCap', 'p4ProgressImg1', 'p4ProgressCap1', 'p4ProgressImg2', 'p4ProgressCap2', 'p4ProgressImg3', 'p4ProgressCap3', 'p4Explanation',
         ]
+
 
--- IMD2009A/portfolio_project/Artfolio/urls.py
+++ IMD2009A/portfolio_project/Artfolio/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
     path('selectTemplate', views.selectTemplate, name='selectTemplate'),
     path('newTemplate', views.newTemplate, name='newTemplate'),
     path('editTemplate/<int:templateId>', views.editTemplate, name='editTemplate'),
+
 ]
